23a178ac7f43dd59b7446e7dff9cf20cb331fa1d
DEVELOPERYUMIN <147611458+DEVELOPERYUMIN@users.noreply.github.com>
2026-01-20 17:45:19 +0900

    코테 스터디 코드 공유
    Refactor solution function to handle lost and reserve sets more efficiently. Added alternative solutions with comments for clarity.

--- Python3/프로그래머스/1/42862. 체육복/체육복.py
+++ Python3/프로그래머스/1/42862. 체육복/체육복.py
@@ -14,8 +14,7 @@ def solution(n, lost, reserve): (수업 들을 수 있는 학생 수 , 체육복
 
     # 작은 번호부터 빌려주기 >> 그리디  : 왼 (i-1) -> 오 (i+1) 순서로 빌림 
     for x in sorted(lost_set):
-
-   
+        
         # 왼쪽(x-1)을 먼저 확인하는 이유
         # x-1은 번호가 더 작음 이미 for문에서 지나갔거나 앞으로 다시 기회 x  
         
@@ -33,4 +32,50 @@ def solution(n, lost, reserve): (수업 들을 수 있는 학생 수 , 체육복
     return n
 
 
-# discard 는 없어도 오류 안 뜸 
+
+# 다른풀이 1 : 
+# discard 는 remove 와 다르게 없어도 오류 안 뜸 !!! 
+def solution(n, lost, reserve):
+    r_lost = set(lost) - set(reserve)
+    r_reserve = set(reserve) - set(lost)
+    lost_num = len(r_lost)
+    # print(dir(r_lost))
+    for i in r_reserve:
+        if i-1 in r_lost:
+            r_lost.discard(i-1)
+            lost_num -= 1
+        elif i+1 in r_lost:
+            r_lost.discard(i+1)
+            lost_num -= 1
+            
+    return n - lost_num
+
+# 다른 풀이 2 
+def solution(n, lost, reserve):
+    answer = 0
+    #1. set으로 다시 저장하기
+    lost = set(lost)
+    reserve = set(reserve)
+    
+    #2. 교집합(잃어버렸는데 여벌도 있는 학생) 먼저 제거 
+    if lost & reserve:
+        common = lost & reserve #교집합은 한번 변수에 저장해두고 양쪽에서 제거해야함
+        lost = lost - common
+        reserve = reserve - common
+     #3. 이제 빌려주기 (빌려줄 수 있는지 확인)
+    for i in list(lost): #반복 중 set 변경하면 오류나기에 list로 방지 
+        if i - 1 in reserve: # 앞번호 학생이 여벌 있으면 빌림
+            reserve.remove(i-1) # 여벌 사용하기 
+            lost.remove(i) # 체육복 문제 해결
+            
+        elif i + 1 in reserve: # 앞번호 x, 뒷번호 학생 여벌 있으면 빌림
+            reserve.remove(i+1) # 여벌 사용하기 
+            lost.remove(i) # 체육복 문제 해결
+            
+        else: # 앞/뒤 모두 여벌이 없으면 아무것도 못 함  
+            pass
+    # 체육복 입을 수 있는 학생 수 
+    return n - len(lost)
+
+
+
