4c803afcba8ec3b58686cc3fa70aa8c844d4a955
DEVELOPERYUMIN <147611458+DEVELOPERYUMIN@users.noreply.github.com>
2026-01-27 16:07:10 +0900

    Update 구명보트.py

--- Python3/프로그래머스/2/42885. 구명보트/구명보트.py
+++ Python3/프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,3 +1,8 @@
+'''
+무거운 사람은 선택지가 적음, 
+제일가벼운 + 제일 뚱뚱 조합으로 하는 게 최선 (나중에 가벼운사람 2명에서 탈 경우를 고려)
+'''
+
 def solution(people, limit):
     answer = 0
     # 일단정렬
@@ -8,7 +13,7 @@ def solution(people, limit):
     while a<b:
         if people[a]+people[b]<=limit:
             a+=1 # 다음 뚱뚱으로 포인터 바꿈
-            answer+=1 # 태움 
+            answer+=1 # 두명이 한 보트에 탄 횟수 
         b-=1 # 덜뚱뚱
     # return answer : 두명 타는 것만 고려함
     return len(people)-answer
